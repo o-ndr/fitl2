665ae850a867450994778b04324767a545bf51b2
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2017-01-09 14:07:11 -0500

    updtae to About page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>About</title>
+		<title>Aboot</title>
 	</head>
 	<body>
 		<h1>This is the app I am building in the <a href="htttps://selftaughtcoders.com/from-idea-to-launch">From Idea To Launch</a></h1>

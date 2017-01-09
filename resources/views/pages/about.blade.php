9ebcade0be6a9cecd1d417d7d644e4b6c7461aed
vagrant <vagrant@homestead>
2017-01-09 18:58:02 +0000

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>This is the app I am building in the <a href="htttps://selftaughtcoders.com/from-idea-to-launch">From Idea To Launch</a></h1>
+	</body>
+</html>
